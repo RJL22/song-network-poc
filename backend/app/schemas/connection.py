8949ba888b34ae868564148178c85aec01b8516a
Ryan Lee <ryanjlee22@gmail.com>
2026-07-24 10:29:43 -0500

    Fix typo

--- backend/app/schemas/connection.py
+++ backend/app/schemas/connection.py
@@ -8,10 +8,3 @@ class ConnectionResponse(BaseModel):
     id: int
     song_1_id: int
     song_2_id: int
-
-class ConnectionResponse(BaseModel):
-    id: int
-    song_1_id: int
-    song_2_id: int
-
-    model_config = ConfigDict(from_attributes=True)
